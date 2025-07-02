e9be0b1d49b84ada175171251960cbe56250404b
SIFAYET ULLAH <sifayet2004@gmail.com>
2025-07-02 19:41:53 +0600

    added body bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`relative ${nunito.variable} antialiased`}>
+      <body className={`relative ${nunito.variable} antialiased bg-white`}>
         <Navbar />
         {children}
       </body>
